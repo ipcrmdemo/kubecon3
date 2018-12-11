045a75870576668cb9705f960532a6d9035eb9b6
Matt Cadorette <mcadorette@gmail.com>
2018-12-11 13:12:09 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/kubecon3/Kubecon3Application.java
@@ -0,0 +1,12 @@
+package com.kubecon3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kubecon3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kubecon3Application.class, args);
+	}
+}
---
+++ src/test/java/com/kubecon3/Kubecon3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kubecon3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kubecon3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
